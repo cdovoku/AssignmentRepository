17fa10bae89620ea9db2e8c40c2665c8e3434d02
Christine <cdovoku@gmail.com>
2022-06-13 01:38:59 +1200

    My First Ccommits

---
+++ Mission 2/indexx.js
@@ -0,0 +1,25 @@
+function displayTime() {
+  var dateTime = new Date();
+  var hrs = dateTime.getHours();
+  var mins = dateTime.getMinutes();
+  var sec = dateTime.getSeconds();
+  var session = document.getElementById('sessions');
+
+  if (hrs >= 12) {
+    session.innerHTML = 'PM';
+  }else {
+    session.innerHTML = 'AM';
+  }
+
+  if (hrs > 12) {
+    hrs = hrs-12;
+  }
+ 
+ 
+
+  document.getElementById('hours').innerHTML = hrs;
+  document.getElementById('minutes').innerHTML = mins;
+  document.getElementById('seconds').innerHTML = sec;
+}
+
+setInterval(displayTime, 10);
